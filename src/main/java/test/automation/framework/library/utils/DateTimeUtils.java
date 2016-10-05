16776998de59329d39f32178a27313c5826ddd46
sganeshkv <s.ganesh.kv@gmail.com>
2016-10-05 20:27:48 +0530

    Added functions to DateTimmeUtils.java

--- src/main/java/test/automation/framework/library/utils/DateTimeUtils.java
+++ src/main/java/test/automation/framework/library/utils/DateTimeUtils.java
@@ -1,5 +1,23 @@
 package test.automation.framework.library.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTimeUtils {
+	public static Date convertStringtoDate(String date, String sourceDateFormat) throws ParseException {
+		return new SimpleDateFormat().parse(date);
+	}
 	
+	public static String convertDatetoString(Date date) {
+		return date.toString();
+	}
+	
+	public static String returnDateInRequestedFormat(Date date, String format) {
+		return new SimpleDateFormat(format).format(date);
+	}
+	
+	public static String returnDateInRequestedFormat(String date, String sourceFormat, String destFormat) throws ParseException {
+		return new SimpleDateFormat(destFormat).format(convertStringtoDate(date,sourceFormat));
+	}
 }
