44e3e841c972c875ff24cb666b6beff7bbe4dbc2
sganeshkv <s.ganesh.kv@gmail.com>
2016-10-06 21:30:56 +0530

    Added getCellValue to ExcelUtils

--- src/main/java/test/automation/framework/library/utils/ExcelUtils.java
+++ src/main/java/test/automation/framework/library/utils/ExcelUtils.java
@@ -1,13 +1,36 @@
 package test.automation.framework.library.utils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellValue;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
+
+	//TODO
+	//private static final String ConditionSeparator = ";";
+	//private static final String ConditionValueSeparator = "=";
+
+	/**
+	 * <b>createExcel - Creates Excel</b>
+	 * 
+	 * @param filePath
+	 *            - Full Qualified excel Path
+	 * @param sheetnames
+	 *            -names of sheets if any, else default Sheet1
+	 * @return boolean
+	 * @throws Exception
+	 */
 	public static boolean createExcel(String filePath, String... sheetnames) throws Exception {
 		boolean flag = false;
 		String[] sheets;
@@ -16,6 +39,9 @@ public class ExcelUtils {
 
 		if (excelFile.exists())
 			throw new IOException("File " + excelFile.getName() + " already exists. Please check and try again");
+
+		if (!excelFile.getParentFile().exists())
+			excelFile.getParentFile().mkdirs();
 
 		if (sheetnames.length >= 1 && sheetnames != null)
 			sheets = sheetnames;
@@ -44,5 +70,85 @@ public class ExcelUtils {
 			fos.close();
 		}
 		return flag;
+	}
+
+	/**
+	 * 
+	 * @param excelPath
+	 * @param sheetname
+	 * @param rownum
+	 * @param columnnum
+	 * @return Object
+	 * @throws IOException
+	 */
+	@SuppressWarnings("deprecation")
+	public static Object getCellValue(String excelPath, String sheetname, int rownum, int columnnum)
+			throws IOException {
+		Object value = null;
+		Workbook workbook = null;
+		Cell cell = null;
+		File excelPath_f = new File(excelPath);
+		FileInputStream fis = null;
+		CellValue cellvalue = null;
+
+		if (!excelPath_f.exists())
+			throw new FileNotFoundException("File does not exist - " + excelPath);
+
+		fis = new FileInputStream(excelPath_f);
+
+		if (excelPath_f.getName().contains(".xlsx")) {
+			workbook = new XSSFWorkbook(fis);
+			XSSFSheet sheet = ((XSSFWorkbook) workbook).getSheet(sheetname);
+			try {
+				cell = sheet.getRow(rownum).getCell(columnnum);
+			} catch (NullPointerException e) {
+				return null;
+			} finally {
+				workbook.close();
+				fis.close();
+			}
+		} else {
+			workbook = new HSSFWorkbook(fis);
+			HSSFSheet sheet = ((HSSFWorkbook) workbook).getSheet(sheetname);
+			try {
+				cell = sheet.getRow(rownum).getCell(columnnum);
+			} catch (NullPointerException e) {
+				return null;
+			} finally {
+				workbook.close();
+				fis.close();
+			}
+		}
+
+		cellvalue = workbook.getCreationHelper().createFormulaEvaluator().evaluate(cell);
+
+		switch (cellvalue.getCellType()) {
+
+		case Cell.CELL_TYPE_STRING:
+			value = cell.getStringCellValue();
+			break;
+
+		case Cell.CELL_TYPE_NUMERIC:
+			if (DateUtil.isCellDateFormatted(cell)) {
+				value = cell.getDateCellValue();
+			} else {
+				value = cell.getNumericCellValue();
+			}
+			break;
+
+		case Cell.CELL_TYPE_BLANK:
+			value = "";
+			break;
+
+		case Cell.CELL_TYPE_BOOLEAN:
+			value = cell.getBooleanCellValue();
+			break;
+		default:
+			break;
+		}
+
+		workbook.close();
+		fis.close();
+		return value;
 	}
 }
--- src/test/java/test/automation/dummy/DummyTestClass.java
+++ src/test/java/test/automation/dummy/DummyTestClass.java
@@ -4,6 +4,18 @@ import test.automation.framework.library.utils.ExcelUtils;
 
 public class DummyTestClass {
 	public static void main(String[] args) throws Exception {
-		ExcelUtils.createExcel("C:\\Users\\SHANKAR\\Desktop\\Shankar\\tempexcel1.xls");
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 0, 0));
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 1, 0));
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 2, 0));
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 3, 0));
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 4, 0));
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 5, 0));
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 6, 0));
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 7, 0));
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 8, 0));
+		System.out.println(ExcelUtils.getCellValue("one.xlsx", "Sheet1", 18, 0));
+		System.out.println(ExcelUtils.getCellValue("two.xls", "Sheet1", 0, 0));
+		System.out.println(ExcelUtils.getCellValue("two.xls", "Sheet1", 1, 0));
+		System.out.println(ExcelUtils.getCellValue("two.xls", "Sheet1", 2, 0));
 	}
 }
